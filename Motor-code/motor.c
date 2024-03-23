132f2c839e65100c6293923648ff6d8bb4dfa9ee
George <georgelin469@gmail.com>
2024-03-22 17:16:34 -0700

    Commented motor test code

--- Motor-code/motor.c
+++ Motor-code/motor.c
@@ -19,9 +19,9 @@
 //#define LEFT_MOTOR [pinout]
 //#define RIGHT_MOTOR [pinout]
 
-/* UNTESTED - GL
- * Function: adjusts motor 
- * return: float - scaling factor
+/* TESTED ON SOFTWARE - GL
+* Function: adjusts motor 
+* Output: Set 4 pins to either `0, 1, or LEFT_PWM, right_PWM
 */
 
 /* Control Scheme based on joystick position
@@ -66,7 +66,7 @@ float LeftMotorAdjust_angle(float refAngle) {
     return PWM_adjust;
 }
 
-float RightMotorAdjust_angle(float refAngle) {
+float RightMotorAdjust_angle(float refAngle) { 
     float angle = refAngle * 180 / PI;
     float PWM_adjust = 1;
 
@@ -86,78 +86,67 @@ float RightMotorAdjust_angle(float refAngle) {
     return PWM_adjust;
 }
 
-
-/*
-    Function: Returns a PWM value between 65 - 100 based on joy stick position
-    Note: Joystick already returns values between 65 - 100
-    Param: velocity -- 
-    Param: PWM_adjust -- value adjustment to PWM based on angle, between -1 and 1
-*/
+/*  
+ *  Function: Returns a PWM value between 65 - 100 based on joy stick position
+ *  Note: Joystick already returns values between 65 - 100
+ *  Param: velocity -- 
+ *  Param: PWM_adjust -- value adjustment to PWM based on angle, between -1 and 1
+ */
 float get_PWM (float velocity, float PWM_adjust) {
     float PWM;
     float adjust = velocity * fabsf(PWM_adjust) / MAX_VELOCITY; // returns a value between 0 and 1
     
-    // printf("adjust = %f, PWM = %f\n", adjust, PWM);
-    // printf(", in func = %f\n", PWM_adjust);
-    // map 0 to 1 to 65 - 100
+    // map 0 to 1 to MIN_PWM - 100
     PWM = MIN_PWM + (100.00 - MIN_PWM) * adjust; // linear mapping function
     return PWM;
 }
 
 /*
-* Function: Turns on and off transistors based on power adjustment value
-* Param: PWM - between 
-* Return: High/Low value to turn off or on transistors in the h bridge 
+*   Function: Turns on and off transistors based on power adjustment value
+*   Param: PWM - between 
+*   Return: High/Low value to turn off or on transistors in the h bridge 
 */
-int LeftMotorDirection (float PWM, float PWM_adjust) {
-
+void turnLeftMotor (float PWM, float PWM_adjust) {
     // Clock wise rotation
     if (PWM_adjust >= 0) {
-        // LEFT_MOTOR = PWM;
-        // LEFT_GROUND = 0;
+        // LEFT_MOTOR = PWM; // turns when `LEFT_MOTOR` is HIGH
+        // LEFT_GROUND = 0;  // set to 0 when CW
     }
-    else {
-        // LEFT_MOTOR = 100-PWM; // Inversed since 
-        // LEFT_GROUND = 1;
+    // One pin is set to 1
+    // CCW rotation 
+    else { 
+        // LEFT_MOTOR = 100-PWM; // Inversed by 100 - PWM, turns when `LEFT_MOTOR` is LOW
+        // LEFT_GROUND = 1;      // set to 1 when CCW 
     }
-        return 0;    
 }
 
-int RightMotorDirection (float PWM, float PWM_adjust) {
-    if (PWM_adjust >= 0)
-        return 1; /* ---- Keep/change after testing*/
-    else
-        return 0;    
+int turnRightMotor (float PWM, float PWM_adjust) {
+    if (PWM_adjust >= 0) {
+        // RIGHT_MOTOR = PWM;     // turns when `RIGHT_MOTOR` is HIGH
+        // LEFT_GROUND = 0;       // set to 0 when CW
+    }
+    // One pin is set to 1
+    // CW rotation
+    else {
+        // RIGHT_MOTOR = 100-PWM    // Inversed by 100 - PWM, turns when `RIGHT_MOTOR` is LOW
+        // RIGHT_GROUND = 1;        // set to 1 when CCW
+    }
 }
 
 int main (void) {
     // need to add in buffer for the midpoint values
+    double x, y;
 
-    // hardcoded x and y values for testing
-    double x = 1.1199;
-    double y = 1.2288;
-    
-    // the below voltage values are used as zeros
-    double midX = 0.0; // 2.30355 V
-    double midY = 0.0; // 2.37534 V
     double angle;    
     double velocity;
 
-    /* used to replace hardcoded values */
-    // note that x and y (floats in the reading) must be cast to doubles)
-    // calculate the velocity we should be moving at using abs()
-    velocity = sqrt(pow((double)x,2)+pow((double)y,2));
-
-    // calculate the direction in which we should be moving (in radians)
-    /* angle calculation
-        - an if statement to change divide by zero errors
-        - checking values of y and x so that domain is correct
-        - y positive => 0 to 180
-        - y negative => 0 to -180
-    */
-    angle = atan2f(y,x);
-
-    // tests
+    /* Motor Spin Simulation - GL
+     * Param: 0 < t < 2PI
+     * Out: left_adjust  -- adjustment and direction of left motor
+     * Out: right_adjust -- adjustment and direction of right motor
+     * Out: left_PWM     -- PWM on left motor
+     * Out: right_PWM    -- PWM on right motor
+     */
     float t = 0;
     while (t < 2*PI) {
         x = 50.0 * cos (t); // setting imaginary x joystick input
@@ -170,14 +159,13 @@ int main (void) {
         float left_adjust = LeftMotorAdjust_angle(angle);
         float right_adjust = RightMotorAdjust_angle(angle);
         
-        // printf("t = %f , ", t);
-        // printf("in main: %f", left_adjust);
         float left_PWM = get_PWM(velocity, left_adjust);
         float right_PWM = get_PWM(velocity, right_adjust) * RIGHT_MOTOR_ADJUST;
 
+        turnLeftMotor (left_PWM, left_adjust);
+        turnRightMotor(right_PWM, right_adjust);
         
         // printf("%f, %f, %f, %f, %f, %f\n", t, x, y, angle, left_adjust, right_adjust);
-        printf("%f, %f\n",left_PWM, right_PWM);
+        // printf("%f, %f\n",left_PWM, right_PWM);
     }
-    
 }
