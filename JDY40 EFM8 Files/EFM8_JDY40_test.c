5dd20df3e3c12603944747adb381aecbd58f2dab
Maya Taylor <146891846+maya-taylor@users.noreply.github.com>
2024-03-26 15:03:57 -0700

    Update EFM8_JDY40_test.c

--- JDY40 EFM8 Files/EFM8_JDY40_test.c
+++ JDY40 EFM8 Files/EFM8_JDY40_test.c
@@ -229,16 +229,21 @@ void SendATCommand (char * s)
 	printf("Response: %s\r\n", buff);
 }
 
-void main (void)
+
+void main(void)
 {
 	unsigned int cnt;
 	float extract_num = 0.0;
 	float prev_num = 0.1;
 	int mapped_num; //for the beeping
-	float min_metal_detect = 170080.0; //frequency at which metal is detected
+	float min_metal_detect = 200; //frequency increase at which metal is detected
 	float mapped_range = 5.0; //1 to mapped_range+1
 	float extract_range = 800.0; //max range of beeping frequency
-	
+	int timeout_cnt = 0;
+	int sum_count = 0; //count to keep track of first 10 vals
+	int sum_freq = 0; //where I am adding the frequencies into
+	int baseline_freq;
+
 	
 	waitms(500);
 	printf("\r\nJDY-40 test\r\n");
@@ -274,46 +279,86 @@ void main (void)
 	cnt=0;
 	while(1)
 	{
-		if(P3_7==0)
-		{
-			sprintf(buff, "JDY40 test EFM8 %d\r\n", cnt++);
-			sendstr1(buff);
+		//if(P3_7==0)
+		//{
+		//	sprintf(buff, "JDY40 test EFM8 %d\r\n", cnt++);
+		//	sendstr1(buff);
+		//	putchar('.');
+		//	waitms_or_RI1(500);
+		//}
+		//if(RXU1())
+		//{
+		//	prev_num = extract_num;
+		//	getstr1(buff);
+		//	printf("RX: %s\r\n", buff);
+		//	//this is where we print the received info
+		//	//need to take a section of the string
+		//	extract_num = atof(buff);
+		//	if(extract_num < 160000.0 || extract_num > 180000.0)
+		//	{
+		//		extract_num = prev_num;
+		//	}
+		//	printf("Number received: %.2f\r\n", extract_num);
+		//	
+		//	if(extract_num > min_metal_detect)
+		//	{
+		//		printf("BEEP\r\n");
+		//		//take extract_num - 170000 to get the frequency to use for mapping	
+		//		//range of beeping is 1-6 for this
+		//		mapped_num = (extract_num - min_metal_detect)*(mapped_range)/(extract_range)+1;
+		//		//note: due to truncation mapped_num will never be top of the map_range
+		//		// could not get round() function to work -- to try later?
+		//		if(mapped_num > mapped_range)
+		//			mapped_num = mapped_range;
+		//			
+		//	}
+		//	
+		//	else
+		//	{
+		//		mapped_num = 0;
+		//	}
+		//	printf("%d\r\n", mapped_num);
+		//}
+		    waitms_or_RI1(100);
+			sendstr1("M\r\n");
 			putchar('.');
-			waitms_or_RI1(500);
-		}
-		if(RXU1())
-		{
-			prev_num = extract_num;
-			getstr1(buff);
-			printf("RX: %s\r\n", buff);
-			//this is where we print the received info
-			//need to take a section of the string
-			extract_num = atof(buff);
-			if(extract_num < 160000.0 || extract_num > 180000.0)
-			{
-				extract_num = prev_num;
-			}
-			printf("Number received: %.2f\r\n", extract_num);
 			
-			if(extract_num > min_metal_detect)
+			timeout_cnt = 0;
+			while (1) 
 			{
-				printf("BEEP\r\n");
-				//take extract_num - 170000 to get the frequency to use for mapping	
-				//range of beeping is 1-6 for this
-				mapped_num = (extract_num - min_metal_detect)*(mapped_range)/(extract_range)+1;
-				//note: due to truncation mapped_num will never be top of the map_range
-				// could not get round() function to work -- to try later?
-				if(mapped_num > mapped_range)
-					mapped_num = mapped_range;
+				if (RXU1()) break;
+				waitms_or_RI1(1);
+				timeout_cnt++;
+				if (timeout_cnt>=500){
+					printf("timeout \r\n");
+					break;
+				 }
+			}
+			if (RXU1()) 
+			{
+				getstr1(buff);
+				if(strlen(buff)== 10)
+				{
+					//need to test sum_count part still but should work
+					printf("RX: %s\r\n", buff);
+					extract_num = atof(buff);
+					if(sum_count < 10)
+					{
+						sum_freq += extract_num;
+						sum_count++;
+					}
+					if(sum_count == 10)
+						baseline_freq = sum_freq/10;
+					if(sum_count > 10)
+					{
+						//now we can start detecting
+						if(extract_num > min_metal_detect + baseline_freq)
+							printf("Beep\r\n}");
+					}
 					
+				}
+				//printf("LR = %d\r\n", strlen(buff));
 			}
-			
-			else
-			{
-				mapped_num = 0;
-			}
-			printf("%d\r\n", mapped_num);
-		}
-		//waitms_or_RI1(300);
 	}
 }
+
