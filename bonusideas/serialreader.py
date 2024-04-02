3f21d85eae8407606c4161b7a5b5f3094874ca65
keybo! <145713027+p14n0f0rt3@users.noreply.github.com>
2024-04-01 18:11:41 -0700

    Revert "csv generated"
    This reverts commit 5cdabbec8fb4e58a5b44afd5a59d9437423b9525.

--- bonusideas/serialreader.py
+++ bonusideas/serialreader.py
@@ -3,7 +3,7 @@ import csv
 
 # Configure the serial port
 ser = serial.Serial(
-    port='/dev/tty.URT0',  # Change this to your serial port
+    port='COM16',  # Change this to your serial port
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
